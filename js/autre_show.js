fc50993d6a263f151206a1a744b47bcc342d9102
Gabriel Legout <gabriel.legout9@gmail.com>
2026-06-02 14:50:27 +0200

    making link insta proof

--- js/autre_show.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const url = document.URL;
-    var parsedURL = url.split("?pdf=");
-    pdf_link = parsedURL[1];
-    pdfContainer = document.getElementById("pdf-container");
-    pdfContainer.innerHTML = `<embed src="..\\ressources\\${pdf_link}" height="100%" width="100%" type="application/pdf"/>
-`;
-});
